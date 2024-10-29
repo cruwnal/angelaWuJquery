0afad2ce510ed0052c69ec86404d1d6f60715f4a
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-29 07:49:53 +0530

    3rd

--- index.js
+++ index.js
@@ -11,3 +11,9 @@
 // $('button')
 
 $('h1').addClass('big-title .margin-50')
+$('h1').text('bye')
+//$("button").text("do not click me");
+$('button').html('<em>hello</em>');
+console.log($('img').attr('src'));
+$('a').html('<b>ckick here</b>');
+$('a').attr('href','jiohotstar.com');

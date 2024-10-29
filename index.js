3dacdaa6f79d3ee4ba15fdd3f148891a7284886b
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-29 08:45:33 +0530

    4th

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ $('button').html('<em>hello</em>');
 console.log($('img').attr('src'));
 $('a').html('<b>ckick here</b>');
 $('a').attr('href','jiohotstar.com');
+
+$('h1').click(
+    function (){
+        $('h1').css('color','purple')
+    }
+)
+
+
+$('button').click(function (){
+    $('h1').html('Kunal Nagwanshi');
+    $('h1').css('color','purple');
+})
+
+$('input').keypress(function (event){
+console.log(event.key);
+})
+
+
+$(document).keypress(function (event){
+    $('button').html(event.key)
+    })
+
+$('h1').on('mouseover',function(){
+    $('h1').css('color','purple')
+});
+
+$('h1').hover(mouseEnter, mouseLeave);
+function mouseEnter() {
+    $('h1').css('color','purple') ;
+};
+function mouseLeave() {
+    $('h1').css('color','green');
+};

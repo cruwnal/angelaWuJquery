30e5350cd36d3cd83d62c6bc8c1db72b51b22bb2
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-29 07:27:12 +0530

    addclass in jquery

---
+++ index.js
@@ -0,0 +1,13 @@
+// $(document).ready(function (){
+
+//     
+
+// })
+
+
+// $('h1').css('color','red')
+
+// console.log($('h1').css('font-size'))
+// $('button')
+
+$('h1').addClass('big-title .margin-50')
